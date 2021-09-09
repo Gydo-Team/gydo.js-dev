a0d7a2139ab7a4f64019c4c1957cc976164ff37e
loldonut <ricricramisogaming18@gmail.com>
2021-09-09 20:58:44 +0800

    Update v1.0.0

---
+++ src/index.d.ts
@@ -0,0 +1,59 @@
+import { 
+    Client,
+    Message,
+    Presence,
+    Channel,
+    ClientPresenceStatus,
+    Intents
+} from "discord.js";
+
+//#region Class(es)
+export class config {
+    public constructor (va: IConfig);
+    public readonly ping: number;
+    private token: string;
+    public prefix: string;
+    public readonly username: string;
+    public guildMemberAdd(va: IGuildMember): Message;
+    public guildMemberRemove(va: IGuildMember): Message;
+    public currentStatus(status: string, type:ActivityTypes): Presence;
+    public readonly status: string | Presence;
+    public cmd(cmd: ICommand): Message;
+    public MessageDetect(): Message;
+    public loopStatus(arrayOfStatus: string[], time: number, type: ActivityTypes): Presence;
+    public addIntents(int: Intents): Promise<void>;
+    public removeIntents(int: Intents): Promise<void>;
+}
+
+export class ClientUser {
+    public constructor (client: Client);
+    public readonly id: number;
+    public readonly tag: string | ClientUser;
+    public readonly discriminator: string | ClientUser;
+    public readonly createdAt: Date;
+}
+
+// Interfaces
+export interface IConfig {
+    token: string;
+    prefix: string;
+}
+
+export interface IGuildMember {
+    channel: Channel;
+    message: Message;
+}
+
+export type ActivityTypes = 
+    | "PLAYING" 
+    | "LISTENING" 
+    | "STREAMING" 
+    | "WATCHING" 
+    | "COMPETING";
+
+export interface ICommand {
+    name: string;
+    code: string;
+}
+
+//#endregion
